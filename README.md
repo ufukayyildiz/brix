59afa278b5be6e00ce1886274f0a60eaa05fbbb7
Ufuk Ayyildiz <23309320+ufukayyildiz@users.noreply.github.com>
2023-06-27 06:01:40 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Og Image Generator
+
+Checkout: http://dynamic-og-image-generator.vercel.app/
+
---
+++ types.d.ts
@@ -0,0 +1,7 @@
+interface OgImageContent {
+  author: string;
+  title: string;
+  websiteUrl: string;
+  avatar: string;
+  theme: string;
+}
